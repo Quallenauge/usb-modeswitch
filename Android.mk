c0ea49cf3d5047dedef962d89345a7e560c9b1c4
Quallenauge <Hamsi2k@freenet.de>
2014-12-11 21:15:09 +0100

    Adapt libusb path.

--- Android.mk
+++ Android.mk
@@ -4,11 +4,11 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := usb_modeswitch.c
 
-LOCAL_STATIC_LIBRARIES := libusb-1.0 libc
+LOCAL_STATIC_LIBRARIES := libusb libc
 
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
-LOCAL_C_INCLUDES := external/libusb-1.0/libusb
+LOCAL_C_INCLUDES := external/libusb/libusb
 
 LOCAL_MODULE := usb_modeswitch
 
