288a76205285d5b75374b5484da08e314aa497dc
Quallenauge <Hamsi2k@freenet.de>
2014-12-12 20:57:44 +0100

    Using shared libraries instead of a static build.

--- Android.mk
+++ Android.mk
@@ -4,9 +4,11 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := usb_modeswitch.c
 
-LOCAL_STATIC_LIBRARIES := libusb libc
+#LOCAL_STATIC_LIBRARIES := libusb libc
 
-LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_SHARED_LIBRARIES := libusb libc
+
+#LOCAL_FORCE_STATIC_EXECUTABLE := true
 
 LOCAL_C_INCLUDES := external/libusb/libusb
 
